8b3bf5e420ccad099ce8737d6de72f79f6cf378b
cruim <hedgehog147@gmail.com>
2020-01-15 14:36:30 +0300

    fix

--- telegram.py
+++ telegram.py
@@ -54,8 +54,8 @@ def webhook():
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
-    text_responce = api.user_message_processing(telegram_id=message.from_user.id, message=message.text)
-    bot.reply_to(message, text_responce)
+    # text_responce = api.user_message_processing(telegram_id=message.from_user.id, message=message.text)
+    # bot.reply_to(message, text_responce)
 
 
 app.run(host=WEBHOOK_LISTEN,

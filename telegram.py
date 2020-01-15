102e8fcdddaedb7fc9da9bdba550060276bd3ba5
cruim <hedgehog147@gmail.com>
2020-01-15 14:31:46 +0300

    fix

--- telegram.py
+++ telegram.py
@@ -53,6 +53,7 @@ def webhook():
 # Handle all other messages
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
+    bot.reply_to(message, message.text)
     text_responce = api.user_message_processing(telegram_id=message.from_user.id, message=message.text)
     bot.reply_to(message, text_responce)
 

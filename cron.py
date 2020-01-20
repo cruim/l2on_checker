d89899e48fef4d416b17df175fa3b6f4232acc41
cruim <hedgehog147@gmail.com>
2020-01-20 13:11:58 +0300

    fix

--- cron.py
+++ cron.py
@@ -29,8 +29,8 @@ def request_response_processing(task):
             price = tr.find('td', {"class": "right"}).text.replace(" ", "")
             if int(price) <= task.price:
                 user = api.get_user(task.user_id)
-                api.update_scheduller_is_active(task)
                 send_message(chat_id=user.telegram_id, text=' '.join([staff.name, date.text, price]))
+                api.update_scheduller_is_active(task)
                 break
     return False
 
